8c7aa046f327a035c2722355ae612078fb464079
Oleg Semiashkin <olegsemyashkin197@gmail.com>
2025-05-17 23:49:00 +0300

    Update card.js

--- components/ui/card.js
+++ components/ui/card.js
@@ -1,15 +1,16 @@
 import React from 'react'
-export function Card({ children, className }) {
+
+export function Card({ children, className = '' }) {
   return (
-    <div className={\`bg-neutral-800 shadow-md rounded-xl border border-neutral-700 \${className || ""}\`}>
+    <div className={"bg-neutral-800 shadow-md rounded-xl border border-neutral-700 " + className}>
       {children}
     </div>
   )
 }
 
-export function CardContent({ children, className }) {
+export function CardContent({ children, className = '' }) {
   return (
-    <div className={\`p-4 \${className || ""}\`}>
+    <div className={"p-4 " + className}>
       {children}
     </div>
   )
